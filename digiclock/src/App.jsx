090b7d2c93509cbbe5526e75a60158c0b306b1e7
Eswaran A <eswaran.a>
2024-12-18 12:12:21 +0530

    adding digital-clock created using react

---
+++ digiclock/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import DigitalClock from './DigitalClock'
+
+function App() {
+  
+
+  return (
+    <>
+      <DigitalClock/>
+    </>
+  )
+}
+
+export default App
